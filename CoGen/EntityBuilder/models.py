a2009db80ac5ea72716e4b1baf078dc9506e4c53
hugh <he.x.working@gmail.com>
2018-10-11 23:03:46 +0800

    update component

--- CoGen/EntityBuilder/models.py
+++ CoGen/EntityBuilder/models.py
@@ -2,14 +2,19 @@ from django.db import models
 
 
 # Create your models here.
+class Project(models.Model):
+    app_name = models.CharField(max_length=255)
+
+
 class Entity(models.Model):
+    app_project = models.ForeignKey('Project', on_delete=models.CASCADE)
     entity_name = models.CharField(max_length=255, null=True, blank=True)
-    string_fields = models.ManyToManyField('StringField', blank=True)
-    int_fields = models.ManyToManyField('IntField', blank=True)
-    enum_fields = models.ManyToManyField('EnumField', blank=True)
-    date_time_fields = models.ManyToManyField('DateTimeField', blank=True)
-    many_to_many_fields = models.ManyToManyField('ManyToManyField', blank=True)
-    foreign_keys = models.ManyToManyField('ForeignKey', blank=True)
+    # string_fields = models.ManyToManyField('StringField', blank=True)
+    # int_fields = models.ManyToManyField('IntField', blank=True)
+    # enum_fields = models.ManyToManyField('EnumField', blank=True)
+    # date_time_fields = models.ManyToManyField('DateTimeField', blank=True)
+    # many_to_many_fields = models.ManyToManyField('ManyToManyField', blank=True)
+    # foreign_keys = models.ManyToManyField('ForeignKey', blank=True)
 
     def __str__(self):
         return self.entity_name
@@ -19,6 +24,7 @@ class BaseField(models.Model):
     is_null = models.BooleanField(default=True)
     is_blank = models.BooleanField(default=True)
     field_name = models.CharField(max_length=255)
+    entity = models.ForeignKey('Entity', related_name='parent_entity', on_delete=models.CASCADE)
     
     class Meta:
         abstract = True
@@ -32,12 +38,12 @@ class StringField(BaseField):
 
 
 class ManyToManyField(BaseField):
-    link_entity = models.ForeignKey('Entity', on_delete=models.CASCADE)
+    link_entity = models.ForeignKey('Entity', related_name='link_entity', on_delete=models.CASCADE)
 
 
 class ForeignKey(BaseField):
-    link_entity = models.ForeignKey('Entity', on_delete=models.CASCADE)
-    
+    link_entity = models.ForeignKey('Entity', related_name='link_entity', on_delete=models.CASCADE)
+
     
 class IntField(BaseField):
     default_value = models.IntegerField(default=0)
